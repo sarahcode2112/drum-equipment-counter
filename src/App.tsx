b68edb3ab21e8ca5a65654ca63273ebc22d3e9ae
Sarah Warren <swarrencode@gmail.com>
2024-03-04 10:35:40 -0800

    refactor equipment count buttons to new file

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { CymbalEnum, CymbalEnumKey, CymbalCounts } from './types';
-
-function EquipmentCountButton({equipmentName, countDirection, onClick}: {countDirection: 'Up' | 'Down', equipmentName: CymbalEnum, onClick: () => void}){
-  return (
-    <button onClick={onClick}>
-      {(countDirection === 'Up') ? `⬆️ ${equipmentName}` : ` ${equipmentName} ⬇️`} 
-    </button>
-  )
-}
-
-function EquipmentCountButtons({ countCymbals, equipmentName }: {countCymbals: any, equipmentName: CymbalEnumKey}) {
-  return (
-    <div>
-      <EquipmentCountButton
-        countDirection={"Up"}
-        equipmentName={CymbalEnum[equipmentName]}
-        onClick={() => {
-          countCymbals(equipmentName, 1);
-        }}
-      ></EquipmentCountButton>
-      <EquipmentCountButton
-        countDirection={"Down"}
-        equipmentName={CymbalEnum[equipmentName]}
-        onClick={() => {
-          countCymbals(equipmentName, -1);
-        }}
-      ></EquipmentCountButton>
-    </div>
-  );
-}
+import { EquipmentCountButtons } from './EquipmentCountButtons';
 
 function App() {
   const [cymbalCounts, setCymbalCounts] = useState<CymbalCounts>({
---
+++ src/EquipmentCountButtons.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { CymbalEnum, CymbalEnumKey } from './types';
+
+function EquipmentCountButton({ equipmentName, countDirection, onClick }: { countDirection: 'Up' | 'Down'; equipmentName: CymbalEnum; onClick: () => void; }) {
+  return (
+    <button onClick={onClick}>
+      {(countDirection === 'Up') ? `⬆️ ${equipmentName}` : ` ${equipmentName} ⬇️`}
+    </button>
+  );
+}
+export function EquipmentCountButtons({ countCymbals, equipmentName }: { countCymbals: any; equipmentName: CymbalEnumKey; }) {
+  return (
+    <div>
+      <EquipmentCountButton
+        countDirection={"Up"}
+        equipmentName={CymbalEnum[equipmentName]}
+        onClick={() => {
+          countCymbals(equipmentName, 1);
+        }}
+      ></EquipmentCountButton>
+      <EquipmentCountButton
+        countDirection={"Down"}
+        equipmentName={CymbalEnum[equipmentName]}
+        onClick={() => {
+          countCymbals(equipmentName, -1);
+        }}
+      ></EquipmentCountButton>
+    </div>
+  );
+}
